659a27c551631813f116862c42a200d3ce369bf7
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2021-12-03 17:12:47 +0530

    Update script.js
    1. Added EvilBall
    2. EvilBall eats other balls code implemented.
    3. EvilBall is able to move around. 
    
    Pending
    1. Design Evilball better
    2. Increase EvilBall speed
    3. Write logic for new game when current game ends. 
    4.  Optimise code

--- script.js
+++ script.js
@@ -63,20 +63,20 @@ Object.defineProperty(Ball.prototype, 'constructor',
 // remember x, y are center coordinates of ball, and canvas(0,0) is at top left corner.
 Ball.prototype.update = function() {
     // if center of ball + radius touches the right vertical wall of canvas
-    if ((this.x + this.size) >= width) {
+    if ((this.x + this.size) >= width) {     // right vertical
       this.velX = -(this.velX);
     }
     // if center of ball - radius of ball touches the left vertical wall 
     // if we dont substract radius the ball will change direction when center of ball touches wall
-    if ((this.x - this.size) <= 0) {
+    if ((this.x - this.size) <= 0) {         // left vertical
       this.velX = -(this.velX);
     }
   
-    if ((this.y + this.size) >= height) {
+    if ((this.y + this.size) >= height) {     // top horizontal
       this.velY = -(this.velY);
     }
   
-    if ((this.y - this.size) <= 0) {
+    if ((this.y - this.size) <= 0) {          // bottom horizontal
       this.velY = -(this.velY);
     }
   
@@ -100,9 +100,6 @@ Ball.prototype.collisionDetect = function(){
 }//func
 
 
-
-
-
 // Animating the ball
 // Creating the balls 
 let balls = [];
@@ -119,20 +116,97 @@ while(balls.length<10){ // creating 10 balls
              true);                                                                // exists=true
         balls.push(ball);
 }
+
+
+
+
+
+// Modelling EvilCircle
+function EvilCircle(x, y, velX, velY, color, size, exists){
+  Shape.call(this, x, y, velX, velY, exists);
+  this.color = color;
+  this.size = size;
+}
+EvilCircle.prototype.draw = function(){    // we can have the same name function as EvilCircle as we did not inhert Shape's methods into it
+  ctx.beginPath(); 
+  ctx.strokeStyle = this.color; 
+  ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);  
+  ctx.stroke();
+}
+EvilCircle.prototype.update = function(){
+  // if center of ball + radius touches the right vertical wall of canvas
+  if ((this.x + this.size) >= width) {
+    this.x -= this.size;
+  }
+  // if center of ball - radius of ball touches the left vertical wall 
+  // if we dont substract radius the ball will change direction when center of ball touches wall
+  if ((this.x - this.size) <= 0) {
+    this.x += this.size;
+  }
+
+  if ((this.y + this.size) >= height) {   // top horizontal
+    this.y -= this.size;
+  }
+
+  if ((this.y - this.size) <= 0) {        // bottom horizontal
+    this.y += this.size;
+  }
+}
+EvilCircle.prototype.setControls = function(){
+  let _this = this;                              // why needed? Due to some function scope apparently
+  window.onkeydown = function(e) {
+    if (e.key === 'a') {
+      _this.x -= _this.velX;
+    } else if (e.key === 'd') {
+      _this.x += _this.velX;
+    } else if (e.key === 'w') {
+      _this.y -= _this.velY;
+    } else if (e.key === 's') {
+      _this.y += _this.velY;
+    }
+  }
+}
+EvilCircle.prototype.collisionDetect = function(){
+  for(let i=0; i<balls.length; i++){
+      //if they are not the same balls
+      if(balls[i].exists){
+          const dx = this.x - balls[i].x;      // why const? why not let?
+          const dy = this.y - balls[i].y;
+          const distance = Math.sqrt(dx*dx+dy*dy);
+
+          if(distance<=this.size+balls[i].size){
+              balls[i].exists = false;
+          } //if
+      }//if
+  }//for
+}//func
+let EvilBall = new EvilCircle(25, 25, 10, 10, 'rgb(255, 255, 255)', 25, true);
+EvilBall.setControls();
+
+
+
+
 // Animation loop
 function loop() {
-    //setting canvas fill color to semi-transparent black
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-    //This serves to cover up the previous frame's drawing before the next one is drawn. If you don't do this, you'll just see long snakes worming their way around the canvas instead of balls moving! The color of the fill is set to semi-transparent rgba(0,0,0,0.25) in above line, to allow the previous few frames to shine through slightly, producing the little trails behind the balls as they move. If you changed 0.25 to 1, you won't see them at all any more.
-    ctx.fillRect(0, 0, width, height);
-    // Loops through all the balls in the balls array, and runs each ball's draw() and update() function to draw each one on the screen, then do the necessary updates to position and velocity in time for the next frame.
-    for (let i = 0; i < balls.length; i++) {
+  //setting canvas fill color to semi-transparent black
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  //This serves to cover up the previous frame's drawing before the next one is drawn. If you don't do this, you'll just see long snakes worming their way around the canvas instead of balls moving! The color of the fill is set to semi-transparent rgba(0,0,0,0.25) in above line, to allow the previous few frames to shine through slightly, producing the little trails behind the balls as they move. If you changed 0.25 to 1, you won't see them at all any more.
+  ctx.fillRect(0, 0, width, height);
+  // Loops through all the balls in the balls array, and runs each ball's draw() and update() function to draw each one on the screen, then do the necessary updates to position and velocity in time for the next frame.
+  for (let i = 0; i < balls.length; i++) {
+    if(balls[i].exists){
       balls[i].draw();
       balls[i].update();
       balls[i].collisionDetect();
     }
-    // Runs the function again using the requestAnimationFrame() method — when this method is repeatedly run and passed the same function name, it runs that function a set number of times per second to create a smooth animation. This is generally done recursively — which means that the function is calling itself every time it runs, so it runs over and over again.
-    requestAnimationFrame(loop);
+  }
+
+  EvilBall.draw();
+  EvilBall.update();
+  EvilBall.collisionDetect();
+
+  // Runs the function again using the requestAnimationFrame() method — when this method is repeatedly run and passed the same function name, it runs that function a set number of times per second to create a smooth animation. This is generally done recursively — which means that the function is calling itself every time it runs, so it runs over and over again.
+  requestAnimationFrame(loop);
 }
 // calling the func to get the loop started
 loop();
